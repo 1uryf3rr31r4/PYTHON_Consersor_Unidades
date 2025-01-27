8c965be787ecc123e9a067e194b4cdc573b739f6
1uryf3rr31r4 <1uryf3rr31r4@gmail.com>
2025-01-27 15:15:28 -0300

    Entrada de dados e execução da função converterUnidade

---
+++ PYTHON_Conversor_Unidades.py
@@ -0,0 +1,9 @@
+import time
+
+def converterUnidade(unidade_inicial, unidade_final, valor):
+
+unidade_inicial = input('Digite a unidade inical (ex: kg, l, s): ')
+unidade_final = input('Digite a unidade final (ex: g, ml, min): ')
+valor = float(input('Digite o valor a ser convertido: '))
+
+converterUnidade(unidade_inicial, unidade_final, valor)
